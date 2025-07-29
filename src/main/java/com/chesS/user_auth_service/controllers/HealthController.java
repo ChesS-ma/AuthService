b21f9a89fb6b0cf00dc0c71d428f7b3c146f8fc5
Salahjb <salah8jab@gmail.com>
2025-07-29 16:24:04 +0100

    add(spring-web-server) : adding the spring web server and test the with health controller

--- src/main/java/com/chesS/user_auth_service/controllers/HealthController.java
+++ src/main/java/com/chesS/user_auth_service/controllers/HealthController.java
@@ -1,4 +1,13 @@
 package com.chesS.user_auth_service.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HealthController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "Auth Service is running!";
+    }
 }
